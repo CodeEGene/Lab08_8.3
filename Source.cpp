9041d7340c5f9a79fda0066f5b4f4c01313137e9
ericschmidutz <e-schmidutz@onu.edu>
2024-03-28 08:31:00 -0400

    Completed convertRomanToInt function.

--- Source.cpp
+++ Source.cpp
@@ -2,28 +2,69 @@
 
 using namespace std;
 
-int romanCharValue(char r);
+int convertRomanToInt(string s);
 
 int main() {
+
+	cout << convertRomanToInt("MCMLXII");
 
 	return 0;
 }
 
-int romanCharValue(char r) {
-	switch (r) {
-	case 'I':
-		return 1;
-	case 'V':
-		return 5;
-	case 'X':
-		return 10;
-	case 'L':
-		return 50;
-	case 'C':
-		return 100;
-	case 'D':
-		return 500;
-	case 'M':
-		return 1000;
+int convertRomanToInt(string s) {
+	const int ROMAN_LENGTH = s.length();
+	int* lettersInRoman = new int[ROMAN_LENGTH + 1];
+
+	for (int i = 0; i < ROMAN_LENGTH; i++) {
+		switch (s[i]) {
+		case 'I':
+			lettersInRoman[i] = 1;
+			break;
+		case 'V':
+			lettersInRoman[i] = 5;
+			break;
+		case 'X':
+			lettersInRoman[i] = 10;
+			break;
+		case 'L':
+			lettersInRoman[i] = 50;
+			break;
+		case 'C':
+			lettersInRoman[i] = 100;
+			break;
+		case 'D':
+			lettersInRoman[i] = 500;
+			break;
+		case 'M':
+			lettersInRoman[i] = 1000;
+			break;
+		default:
+			cerr<<"There was a typo or something.";
+			exit(1);
+			break;
+		}
 	}
+
+	lettersInRoman[ROMAN_LENGTH] = 0;
+
+	int total = 0;
+	int previousValue = 0;
+
+	if (ROMAN_LENGTH == 1) {
+		return lettersInRoman[0];
+	}
+
+	for (int i = 0; i < ROMAN_LENGTH; i++) {
+		if (lettersInRoman[i] >= lettersInRoman[i + 1]) {
+			total += lettersInRoman[i];
+		}
+		else {
+			total += lettersInRoman[i + 1] - lettersInRoman[i];
+			lettersInRoman[i + 1] = 0;
+		}
+	}
+	delete[] lettersInRoman;
+
+	return total;
+
 }
